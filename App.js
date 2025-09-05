9a0216d4d093a0635e772bf21bdf2ad340de2f72
unknown <gugaaroodrigues@gmail.com>
2025-09-05 17:12:08 -0300

    Finalizando o cronometro, app finalizado, deixarei o material para, possivel estudo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React,{Component} from "react";
-import{
+import React, { Component } from "react";
+import {
     View,
     Text,
     StyleSheet,
@@ -7,75 +7,108 @@ import{
     TouchableOpacity
 } from 'react-native';
 
-class App extends Component{
+class App extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            numero: 0
-        };
+            numero: 0,
+            botao: 'VAI',
+            ultimo: null
 
+        };
+        //variavel do timer do relogio
+        this.timer = null;
         this.vai = this.vai.bind(this);
         this.limpar = this.limpar.bind(this);
-    }
-
-    vai(){
-
-        setInterval( ()=> {
-            this.setState({ numero: this.state.numero + 0.1 })
-        }, 100);
-    }
-
-    limpar(){
-
 
     }
 
-    render(){
-        return(
-            <View style= {styles.container}>
+    vai() {
 
-            <Image
-            source={require('./src/cronometro.png')}
-            style={styles.cronometro}
-            />
+        if (this.timer != null) {
 
-            <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>
+            //aqui para o time
+            clearInterval(this.timer);
+            this.timer = null;
+            this.setState({ botao: 'VAI' })
 
-            <View style={styles.btnArea}>
+        } else {
+            //começa a virar o timer
+            this.timer = setInterval(() => {
+                this.setState({ numero: this.state.numero + 0.1 })
+            }, 100);
 
-                <TouchableOpacity style={styles.btn} onPress={this.vai} >
-                    <Text style={styles.btnTexto}>VAI</Text>
-                </TouchableOpacity>
+            this.setState({ botao: 'PARAR' });
+        }
+    }
 
-                <TouchableOpacity style={styles.btn} onPress={this.limpar}>
-                    <Text style={styles.btnTexto}>LIMPAR</Text>
-                </TouchableOpacity>
-            </View>
+    limpar() {
+        if (this.timer != null) {
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+        this.setState({
+            ultimo: this.state.numero,
+            numero: 0,
+            botao: 'VAI'
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                <Image
+                    source={require('./src/cronometro.png')}
+                    style={styles.cronometro}
+                />
+
+                <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>
+
+                <View style={styles.btnArea}>
+
+                    <TouchableOpacity style={styles.btn} onPress={this.vai} >
+                        <Text style={styles.btnTexto}>{this.state.botao}</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+                        <Text style={styles.btnTexto}>LIMPAR</Text>
+                    </TouchableOpacity>
+
+                </View>
+
+                <View style={styles.areaUltima}>
+                    <Text style={styles.textoCorrida}>
+                         {this.state.ultimo > 0 ? 'Ultimo tempo: ' + this.state.ultimo.toFixed(2) + 's' : ''}
+                         </Text>
+
+                </View>
+
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#00aeef'
     },
-    timer:{
+    timer: {
         marginTop: -160,
         color: '#FFF',
         fontSize: 65,
         fontWeight: 'bold'
     },
-    btnArea:{
+    btnArea: {
         flexDirection: 'row',
         marginTop: 70,
         height: 40
     },
-    btn:{
+    btn: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
@@ -84,10 +117,19 @@ const styles = StyleSheet.create({
         margin: 17,
         borderRadius: 20
     },
-    btnTexto:{
+    btnTexto: {
         fontSize: 20,
         fontWeight: 'bold',
         color: '#00aeef'
+    },
+
+    areaUltima:{
+        marginTop: 40
+    },
+    textoCorrida:{
+        fontSize: 23,
+        fontStyle: 'italic',
+        color: '#FFF'
     }
 });
 
